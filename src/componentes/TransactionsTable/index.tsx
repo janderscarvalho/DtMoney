f59a3053c1ea1315ef3b9f3e649a46a8389f5283
Jander Stival de Carvalho <janderscarvalho@gmail.com>
2023-03-01 11:30:26 -0300

    Continuação
    Continuação de estudos Reactjs
    
    Co-Authored-By: jander <jander.sy@163.com>

--- src/componentes/TransactionsTable/index.tsx
+++ src/componentes/TransactionsTable/index.tsx
@@ -1,6 +1,27 @@
+import { useEffect, useState } from "react";
+import { api } from "../../services/api";
 import { Container } from "./styles";
 
+interface Transaction {
+    id: number;
+    title: string;
+    amount: number;
+    type: string;
+    category: string;
+    createdAt: string;
+}
+
 export function TransictionTable (){
+    const [transactions, setTransactions] = useState<Transaction[]>([]);
+
+
+    useEffect(() =>{
+        api.get('transactions')
+        .then(response => setTransactions(response.data.transactions))
+    }, []);
+
+
+
 return (
     <Container>
         <table>
@@ -14,20 +35,27 @@ return (
             </thead>
 
             <tbody>
-                <tr>
-                    <td> Desenvolvimento de Website </td>
-                    <td className="deposit"> R$12.000</td>
-                    <td> Desenvolvimento</td>
-                    <td> 20/02/2021</td>
-                </tr>
+             {transactions.map(transaction =>(
+                <tr key={transaction.id}>
+                  <td>{transaction.title}</td>
 
-                <tr>
-                    <td> Aluguel </td>
-                    <td className="withdraw">- R$1.100</td>
-                    <td> Casa</td>
-                    <td> 17/02/2021</td>
-                </tr>
+                  <td className={transaction.type}>
+                    {new Intl.NumberFormat('pt-BR',{
+                        style:'currency',
+                        currency: 'BRL'
+                    }).format(transaction.amount)}
+                    </td>
 
+                  <td>{transaction.category}</td>
+                  
+                  <td>
+                  {new Intl.DateTimeFormat('pt-BR').format(
+                    new Date(transaction.createdAt)
+                  )}
+                </td>
+
+                </tr>
+             ))}
             </tbody>
         </table>
     </Container>
--- src/componentes/TransactionsTable/styles.ts
+++ src/componentes/TransactionsTable/styles.ts
@@ -6,6 +6,7 @@ export const Container = styled.div`
     table {
         width: 100%;
         border-spacing: 0 0.5rem;
+        padding: 2rem;
 
         th {
             color: var(--text-body);
