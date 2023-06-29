d2bf79fa07d4ead4bf1fd03a5a637bdba090b72d
Jander Stival de Carvalho <janderscarvalho@gmail.com>
2023-06-29 12:04:05 -0300

    Inicial Commit

--- src/componentes/NewTransactionModal/index.tsx
+++ src/componentes/NewTransactionModal/index.tsx
@@ -6,8 +6,10 @@ import closeImg from '../../assets/close.svg';
 import incomeImg from '../../assets/income.svg';
 import outcomeImg from '../../assets/outcome.svg';
 
-import { Container, TransactionTypeContainer, RadioBox } from './styles';
 import { useTransactions } from '../../hooks/useTransactions';
+
+import { Container, TransactionTypeContainer, RadioBox } from './styles';
+
 
 
  
